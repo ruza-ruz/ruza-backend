87dd70a5e7546d93fbeca0dd44b3bae752f9b5d7
ruza-ruz <feyzolahi1362@gmail.com>
2026-02-08 15:55:53 +0330

    Update server.js

--- server.js
+++ server.js
@@ -118,7 +118,7 @@ app.post("/api/chat", async (req, res) => {
           "Content-Type": "application/json"
         },
         body: JSON.stringify({
-          model: "llama-3.1-70b-versatile",
+          model: "llama-3.1-8b-instant",
           max_tokens: 512,
           messages: [
             {
